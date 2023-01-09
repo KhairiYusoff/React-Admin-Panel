a999b3eb9753bd771b99cdeecb08e3a8c533dcf5
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2023-01-10 00:08:07 +0800

    update

--- src/pages/posts/list.tsx
+++ src/pages/posts/list.tsx
@@ -1,7 +1,20 @@
 import { List, TextField, TagField, DateField, Table, useTable } from "@pankod/refine-antd";
-import { IPost } from "interfaces"
+import { useMany } from "@pankod/refine-core";
+import { ICategory, IPost } from "interfaces"
+
 export const PostList: React.FC = () => {
     const { tableProps } = useTable<IPost>();
+
+    const categoryIds =
+        tableProps?.dataSource?.map((item) => item.category.id) ?? [];
+    const { data: categoriesData, isLoading } = useMany<ICategory>({
+        resource: "categories",
+        ids: categoryIds,
+        queryOptions: {
+            enabled: categoryIds.length > 0,
+        }
+    });
+
     return (
         <List>
             <Table {...tableProps} rowKey="id">
@@ -15,6 +28,24 @@ export const PostList: React.FC = () => {
                     dataIndex="createdAt"
                     title="createdAt"
                     render={(value) => <DateField format="LLL" value={value} />}
+                />
+                <Table.Column
+                    dataIndex={["category", "id"]}
+                    title="category"
+                    render={(value) => {
+                        if (isLoading) {
+                            return <TextField value="Loading..." />
+                        }
+                        return (
+                            <TextField
+                                value={
+                                    categoriesData?.data.find(
+                                        (item) => item.id === value,
+                                    )?.title
+                                }
+                            />
+                        )
+                    }}
                 />
             </Table>
         </List>
