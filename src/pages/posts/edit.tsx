63e4d21212420b730cec039040a4fc034f36fe17
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2023-02-04 14:19:14 +0800

    update

--- src/pages/posts/edit.tsx
+++ src/pages/posts/edit.tsx
@@ -14,5 +14,57 @@ export const PostEdit: React.FC = () => {
         resource: "categories",
         defaultValue: queryResult?.data?.data?.category.id
     })
-    return <div></div>
+    return (
+        <Edit saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Title"
+                    name="title"
+                    rules={[
+                        {
+                            required: true
+                        }
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Status"
+                    name="status"
+                    rules={[
+                        {
+                            required: true
+                        }
+                    ]}>
+                    <Select
+                        options={[
+                            {
+                                label: "Published",
+                                value: "published"
+                            },
+                            {
+                                label: "Draft",
+                                value: "draft"
+                            },
+                            {
+                                label: "Rejected",
+                                value: "rejected"
+                            },
+                        ]}
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Category"
+                    name={["category", "id"]}
+                    rules={[
+                        {
+                            required: true
+                        }
+                    ]}
+                >
+                    <Select {...categorySelectProps} />
+                </Form.Item>
+            </Form>
+        </Edit>
+    )
 }
