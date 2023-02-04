6b5c12711f5287ff5b4370a0fe1e33477f314744
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2023-02-04 13:48:27 +0800

    update

--- src/pages/posts/edit.tsx
+++ src/pages/posts/edit.tsx
@@ -1,3 +1,18 @@
+import {
+    useForm,
+    Form,
+    Input,
+    Select,
+    Edit,
+    useSelect
+} from "@pankod/refine-antd"
+import { IPost } from "interfaces"
+
 export const PostEdit: React.FC = () => {
+    const { formProps, saveButtonProps, queryResult } = useForm<IPost>()
+    const { selectProps: categorySelectProps } = useSelect<IPost>({
+        resource: "categories",
+        defaultValue: queryResult?.data?.data?.category.id
+    })
     return <div></div>
 }
