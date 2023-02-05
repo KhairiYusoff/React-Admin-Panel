b38ce8d61516f9a806e5781fa031b115d731754f
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2023-02-05 13:01:15 +0800

    update

--- src/pages/posts/create.tsx
+++ src/pages/posts/create.tsx
@@ -1,3 +1,13 @@
+import {
+    Create,
+    Form,
+    Input,
+    Select,
+    useForm,
+    useSelect
+} from "@pankod/refine-antd"
+import { IPost } from "interfaces"
+
 export const PostCreate: React.FC = () => {
     return <div></div>
 }
